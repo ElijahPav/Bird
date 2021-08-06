8e3ede8dba88bdda1cee81fd600c088e8a551ddf
Konstantin Zhuk <konstantin.zhuk@easybrain.com>
2021-08-06 18:18:05 +0300

    and a one more thing!

--- Assets/Scripts/BirdBehaviour.cs
+++ Assets/Scripts/BirdBehaviour.cs
@@ -1,43 +1,41 @@
 ﻿using UnityEngine;
+
 public class BirdBehaviour : MonoBehaviour
 {
-    [SerializeField]private float horizontalSpeed; 
-    [SerializeField]private float verticalForce;
+    [SerializeField] private float horizontalSpeed;
+    [SerializeField] private float verticalForce;
     [SerializeField] private float verticalForceAfterWallCollision;
-    private float _direction= 1;
+    private float direction = 1;
     private Rigidbody2D rb;
-    private string wallTag = "Wall"; 
-    
-    
+    private const string wallTag = "Wall"; //если переменная не изменяется, то лучше ее помаркать const
+
     private void Awake()
     {
-        rb=GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     private void FixedUpdate()
     {
-        //если работаешь с rigidbody, но менять вручную ускорение так себе идея
-        //и я пока не знаю что предложить в данном случае, мб зафиксировать в rgb Ox и двигать прямолинейно в апдейте
-        rb.velocity = new Vector2(horizontalSpeed*_direction, rb.velocity.y);
-    } 
-    
+        //todo: подумать насчет логики передвижения без rigidbody
+        rb.velocity = new Vector2(horizontalSpeed * direction, rb.velocity.y);
+    }
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb.velocity = new Vector2(horizontalSpeed*_direction, 0);
+            rb.velocity = new Vector2(horizontalSpeed * direction, 0);
             rb.AddForce(Vector2.up * verticalForce, ForceMode2D.Impulse);
         }
     }
-    
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        
         if (collision.gameObject.CompareTag(wallTag))
         {
-            _direction *= -1;
-            rb.velocity = new Vector2(horizontalSpeed*_direction, 0);
-            rb.AddForce(Vector2.up*verticalForceAfterWallCollision, ForceMode2D.Impulse);
+            direction *= -1;
+            rb.velocity = new Vector2(horizontalSpeed * direction, 0);
+            rb.AddForce(Vector2.up * verticalForceAfterWallCollision, ForceMode2D.Impulse);
         }
     }
 }
