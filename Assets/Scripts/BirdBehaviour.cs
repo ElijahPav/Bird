f76b670fa10412bb7ae6e0296f22e23b0b483388
ElijahPav <il.pavl.2002@gmail.com>
2021-08-11 16:07:29 +0300

    add "play" button, add death system, add bird sprite

--- Assets/Scripts/BirdBehaviour.cs
+++ Assets/Scripts/BirdBehaviour.cs
@@ -7,7 +7,8 @@ public class BirdBehaviour : MonoBehaviour
     [SerializeField] private float verticalForceAfterWallCollision;
     private float direction = 1;
     private Rigidbody2D rb;
-    private const string wallTag = "Wall"; //если переменная не изменяется, то лучше ее помаркать const
+    private const string wallTag = "Wall";
+    private const string spikeTag = "Spike";
 
     private void Awake()
     {
@@ -33,9 +34,16 @@ public class BirdBehaviour : MonoBehaviour
     {
         if (collision.gameObject.CompareTag(wallTag))
         {
+            transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y + 180 * direction, 0);
             direction *= -1;
             rb.velocity = new Vector2(horizontalSpeed * direction, 0);
             rb.AddForce(Vector2.up * verticalForceAfterWallCollision, ForceMode2D.Impulse);
+        }
+
+        if (collision.gameObject.CompareTag(spikeTag))
+        {
+            GameManager.manager.PlayerDeth();
+            Debug.Log("Шип");
         }
     }
 }
